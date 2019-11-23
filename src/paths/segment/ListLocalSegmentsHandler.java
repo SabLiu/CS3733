178132a3f711c15b9fc95c5fde0053ff4f94c5a7
mariasharman137 <46602549+mariasharman137@users.noreply.github.com>
2019-11-23 17:22:22 -0500

    Local Segments Handler written - not tested

--- src/paths/segment/ListLocalSegmentsHandler.java
+++ src/paths/segment/ListLocalSegmentsHandler.java
@@ -1,16 +1,44 @@
 package paths.segment;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import databases.SegmentsDAO;
+import definitions.Response;
 import definitions.Segment;
 
- class ListLocalSegmentsHandler implements RequestHandler<Object, Segment[]>{
+ class ListLocalSegmentsHandler implements RequestHandler<Object, Response<Segment[]>>{
 
+	public LambdaLogger logger;
+
+	/** Load from RDS, if it exists
+	 * 
+	 * @throws Exception 
+	 */
+	Segment[] getSegments() throws Exception {
+			logger.log("in getSegments");
+			SegmentsDAO dao = new SegmentsDAO();
+			Segment[] s = {};
+			return dao.getAllLocalSegments().toArray(s);
+		}
+	
 	@Override
-	public Segment[] handleRequest(Object input, Context context) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Response<Segment[]> handleRequest(Object input, Context context) {
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler to list all segments");
 
+		Segment[] list;
+		Response<Segment[]> response;
+		
+		try {
+			list = getSegments();	
+			response = new Response<Segment[]>(list, 200);
+		} catch (Exception e) {
+			response = new Response<Segment[]>(400, e.getMessage());
+		}
+		
+		return response;
+	}
+	
 }
