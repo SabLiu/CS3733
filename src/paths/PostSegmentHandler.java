ab911646fafed4cfffa50c5f3a8ac316e3e3e2b6
mariasharman137 <46602549+mariasharman137@users.noreply.github.com>
2019-11-23 13:36:09 -0500

    Turned project into maven project to import AWS RequestHandler

--- src/paths/PostSegmentHandler.java
+++ src/paths/PostSegmentHandler.java
@@ -1,12 +1,19 @@
 package paths;
 
-//import com.amazonaws.regions.Regions;
-//import com.amazonaws.services.lambda.runtime.Context;
-//import com.amazonaws.services.lambda.runtime.LambdaLogger;
-//import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import definitions.Segment;
 
 
-public class PostSegmentHandler {//implements RequestHandler<Segment,Segment[]>{
+public class PostSegmentHandler implements RequestHandler<Segment,Segment[]>{
+
+	
+	@Override
+	public Segment[] handleRequest(Segment input, Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
