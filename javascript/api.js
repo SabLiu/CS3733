beb3fbfc34098af45aa039dd6377fbce942d9191
Erich <efschwarzrock@wpi.edu>
2019-11-25 18:36:59 -0500

    Fixed HTML to run the initalize function

--- javascript/api.js
+++ javascript/api.js
@@ -1,4 +1,4 @@
 // all access driven through BASE. Must end with a SLASH
 var base_url = "https://xkdxootdqe.execute-api.us-east-2.amazonaws.com/HotspurAPI"; 
 
-var list_local_segments_url = base_url + "localSegments"; //GET
+var list_local_segments_url = base_url + "/localSegments"; //GET
