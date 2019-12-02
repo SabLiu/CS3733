2d2cc4ff229f9269b57845e2682f33edcc6fac50
Erich <efschwarzrock@wpi.edu>
2019-12-02 17:43:51 -0500

    Search CHaracter by name

--- test/databases/SegmentsDAOTest.java
+++ test/databases/SegmentsDAOTest.java
@@ -156,5 +156,87 @@ public class SegmentsDAOTest {
 			assertTrue(difference == 1 && oneAffected);
 		}
 	}
+	
+	@Test
+	public void searchCharacterTest() {
+		List<Segment> controllerSegments = new ArrayList<>();
+		List<Id> id = new ArrayList<>();
+		id.add(new Id("1dba4225-9077-450e-9c94-21f2eaba4e7b.ogg"));
+		id.add(new Id("3c4bdc3a-a3f5-4f39-bf3a-b7f65fa9399b.ogg"));
+		id.add(new Id("3e3b9c56-1a2d-45ed-b676-29de0f4e4486.ogg"));
+		id.add(new Id("525b2e69-5140-47d9-b2d5-ff7bb9f67d5e.ogg"));
+		id.add(new Id("72ba5621-9180-4976-b24d-93cdc98ff6cc.ogg"));
+		id.add(new Id("c9314e2c-68df-48ec-af09-de17bac46ecd.ogg"));
+		id.add(new Id("cf7ccbaa-321f-4365-ab2d-1aabc7354f23.ogg"));
+		id.add(new Id("d16d709b-5b90-48f8-a3c4-57acb0062a0c.ogg"));
+		id.add(new Id("d4117ef5-72bf-46c3-be31-d6a1f275194a.ogg"));
+		id.add(new Id("f599c86f-e60b-44d7-a231-7a3d7f9ff6cc.ogg"));
+		
+		controllerSegments.add(new Segment(id.get(0), false, "You had a normal emotion", "McCoy", "Two.ogg"));
+		controllerSegments.add(new Segment(id.get(1), false, "Then I need a drink", "McCoy", "One.ogg"));
+		controllerSegments.add(new Segment(id.get(5), false, "Crazy way to travel, spreading a man's molecules all over the universe.", "McCoy", "Ten.ogg"));
+		
+		controllerSegments.add(new Segment(id.get(2), false, "Colloquially expressed, but essentially correct.", "Spock", "Eight.ogg"));
+		controllerSegments.add(new Segment(id.get(4), false, "Do you smell something?", "Spock", "Six.ogg"));
+		controllerSegments.add(new Segment(id.get(6), false, "Kindness, Mr. Garrovick, is another human emotion, and I believe we have enough of that.", "Spock", "Five.ogg"));
+		controllerSegments.add(new Segment(id.get(9), false, "I know you would prefer to wallow in a pool of emotion", "Spock", "Four.ogg"));
+		
+		controllerSegments.add(new Segment(id.get(3), false, "Engineering to Captain Kirk", "Scotty", "Nine.ogg"));
+		
+		controllerSegments.add(new Segment(id.get(7), false, "Mr. Spock, why aren’t you dead?", "Kirk", "Seven.ogg"));
+		controllerSegments.add(new Segment(id.get(8), false, "You know, self-pity is a terrible first course", "Chapel", "Three.ogg"));
+		SegmentDAO tester = new SegmentDAO();
+		
+		List<Segment> McCoyControllerSegments = new ArrayList<>();
+		McCoyControllerSegments.add(controllerSegments.get(0));
+		McCoyControllerSegments.add(controllerSegments.get(1));
+		McCoyControllerSegments.add(controllerSegments.get(2));
+		
+		List<Segment> SpockControllerSegments = new ArrayList<>();
+		SpockControllerSegments.add(controllerSegments.get(3));
+		SpockControllerSegments.add(controllerSegments.get(4));
+		SpockControllerSegments.add(controllerSegments.get(5));
+		SpockControllerSegments.add(controllerSegments.get(6));
+		
+		List<Segment> ScottyControllerSegments = new ArrayList<>();
+		ScottyControllerSegments.add(controllerSegments.get(7));
+		
+		List<Segment> gottenSegments = new ArrayList<>();
+		
+		try {
+			gottenSegments = tester.searchSegmentCharacter("McCoy");
+			int i = 0;
+			boolean ta = true;
+			while(i<gottenSegments.size()) {
+				if(!gottenSegments.get(i).equals(McCoyControllerSegments.get(i))) {
+					ta = false;
+				}
+				i++;
+			}
+			gottenSegments = tester.searchSegmentCharacter("Spock");
+			i = 0;
+			boolean tb = true;
+			while(i<gottenSegments.size()) {
+				if(!gottenSegments.get(i).equals(SpockControllerSegments.get(i))) {
+					tb = false;
+				}
+				i++;
+			}
+			gottenSegments = tester.searchSegmentCharacter("Scotty");
+			i = 0;
+			boolean tc = true;
+			while(i<gottenSegments.size()) {
+				if(!gottenSegments.get(i).equals(ScottyControllerSegments.get(i))) {
+					tc = false;
+				}
+				i++;
+			}
+			gottenSegments = tester.searchSegmentCharacter("bill");
+			boolean td = gottenSegments.size() == 0;
+			assertTrue(ta && tb && tc && td);
+		}catch(Exception e) {
+			fail("exception");
+		}
+	}
 
 }
