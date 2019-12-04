9d63ea0fa3f93ed42cde56a72526b1053fb33e59
mariasharman137 <46602549+mariasharman137@users.noreply.github.com>
2019-12-04 13:51:09 -0500

    local test for mark and unmark segments passed

--- src/paths/segment/MarkSegmentHandler.java
+++ src/paths/segment/MarkSegmentHandler.java
@@ -1,17 +1,36 @@
 package paths.segment;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import databases.SegmentDAO;
 import definitions.Id;
+import definitions.Response;
 import definitions.Segment;
 
- class MarkSegmentHandler implements RequestHandler<Id, Segment>{
-
+public class MarkSegmentHandler implements RequestHandler<Id, Response<Segment[]>>{
+	LambdaLogger logger;
+	SegmentDAO dao;
+		
 	@Override
-	public Segment handleRequest(Id input, Context context) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Response<Segment[]> handleRequest(Id id, Context context) {
+		logger = context.getLogger();
+		logger.log(id.toString());
 
+		Response<Segment[]> response;
+		dao = new SegmentDAO();
+		try {
+			if (dao.markSegment(id, true)) {
+				response = new Response<Segment[]>(ListLocalSegmentsHandler.getSegments(logger, dao), 200);
+			} else {
+				response = new Response<Segment[]>(400, "Segment not marked public");
+			}
+		} catch(Exception e) {
+			response = new Response<Segment[]>(400, "Unable to complete request: " +  "(" + e.getMessage() + ")");
+	
+		}
+		return response; 
+	
+	}
 }
