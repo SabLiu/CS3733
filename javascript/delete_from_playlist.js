eccdf6d6065beafd1eebf55d261ccaaf6e31a8cd
SabLiu <sabrinatliu@gmail.com>
2019-12-09 14:46:06 -0500

    scaffold for delete from playlist

---
+++ javascript/delete_from_playlist.js
@@ -0,0 +1,42 @@
+// similar to append_to_playlist
+function processDeleteFromPlaylist(segID) {
+	
+	var data = {};
+	// need to make ID objects for segment and playlist
+	data["segmentId"] = {id: segID}; 
+	data["playlistId"]= {id: currentPlaylistID}; 
+	
+	console.log("currentPlaylistID in append: " + currentPlaylistID);
+	  
+	var js = JSON.stringify(data);
+	
+	console.log("JS:" + js);
+	//UNCOMMENT THIS STUFF WHEN THE HANDLER IS WORKING 
+	
+	
+//	var xhr = new XMLHttpRequest();
+//	xhr.open("POST", delete_from_playlist_url, true);
+//	
+//	xhr.send(js);
+//
+//  // This will process results and update HTML as appropriate. 
+//   
+//	 xhr.onloadend = function () {
+//		  console.log(xhr);
+//		  console.log(xhr.request);
+//		  if (xhr.readyState == XMLHttpRequest.DONE) {
+//			  if (xhr.status == 200) {
+//				  console.log ("XHR:" + xhr.responseText);
+//				  processViewPlaylistResponse(xhr.responseText);
+//				  // depending on JSON response. 
+//			  } else {
+//				  console.log("actual:" + xhr.responseText)
+//				  var js = JSON.parse(xhr.responseText);
+//				  var err = js["error"];
+//				  alert (err);
+//			  }
+//		  } else {
+//			  processViewPlaylistResponse("N/A");
+//		  }
+//	  };
+	}
